416cf9e0fb2c04e2ce1b4c791b78989573a0a144
ignitesparkwebsolutions <er.parthapratimdas@gmail.com>
2024-11-30 18:24:14 +0530

    banner modifications

--- src/script.js
+++ src/script.js
@@ -16,3 +16,44 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
         document.getElementById('contact-form').reset(); // Clear the form
     }
 });
+
+let currentIndex = 0;
+
+function moveSlide(step) {
+    const slides = document.querySelectorAll('.carousel-images div img');
+    const totalSlides = slides.length;
+    currentIndex = (currentIndex + step + totalSlides) % totalSlides;
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
+
+document.querySelectorAll('.carousel-images div').forEach(item => {
+    const screenHeight = window.innerHeight;
+    const screenWidth = window.innerWidth;
+    item.style.height = (screenHeight/2) + 'px';
+    item.style.width = screenWidth + 'px';
+});
+
+document.querySelectorAll('.carousel-images div img').forEach(item => {
+    const screenHeight = window.innerHeight;
+    const screenWidth = window.innerWidth;
+    item.style.height = (screenHeight/2) + 'px';
+    const elemWidth = document.querySelector('.carousel-images div img').width;
+    if (screenWidth < elemWidth) {
+        item.style.width = (screenWidth - 60) + 'px';
+    }
+    let marginVal = ((screenWidth - elemWidth)/2)-8;
+    marginVal = (marginVal < 20) ? 20 : marginVal;
+    item.style.margin = '0px ' + marginVal + 'px';
+});
+
+function autoMoveSlide() {
+    moveSlide(1);
+    setTimeout(() => {
+        autoMoveSlide();
+    }, 3000);
+}
+
+setTimeout(() => {
+    autoMoveSlide();
+}, 3000);
